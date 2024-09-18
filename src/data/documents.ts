e16f7561ad10ed1e77ebab6337864e0dfd225415
Levente Otta <leventeotta@gmail.com>
2024-09-18 09:26:42 +0200

    Update Documents

--- src/data/documents.ts
+++ src/data/documents.ts
@@ -5,36 +5,56 @@ interface Document {
 
 const DOCUMENTS: Array<Document> = [
   {
-    name: "GDPR - Adatkezelési hozzájárulási nyilatkozat",
-    href: "/documents/GDPR%20nyilatkozat.pdf",
-  },
-  {
-    name: "GDPR - Szabályzata",
-    href: "/documents/GDPR%20szabalyzat.pdf",
-  },
-  {
-    name: "1 %",
-    href: "/documents/2022_adoszam.pdf",
+    name: "Alapszabályzat",
+    href: "/documents/BASE%20alapszabalyzata.pdf",
   },
   {
     name: "Házirend",
     href: "/documents/hazirend.pdf",
   },
   {
-    name: "Alapszabályzat",
-    href: "/documents/BASE%20alapszabalyzata.pdf",
-  },
-  {
-    name: "Belépési kérelem",
-    href: "/documents/tagfelveteltkerolap.pdf",
+    name: "Tagnyilvántartó adatlap",
+    href: "/documents/tagnyilvantartoadatlap.pdf",
   },
   {
     name: "Pártoló tagsági kérelem",
     href: "/documents/partoloitagfelveteltkerolap.pdf",
   },
   {
-    name: "Tagnyilvántartó adatlap",
-    href: "/documents/tagnyilvantartoadatlap.pdf",
+    name: "Belépési kérelem",
+    href: "/documents/tagfelveteltkerolap.pdf",
+  },
+  {
+    name: "GDPR - Szabályzata",
+    href: "/documents/GDPR%20szabalyzat.pdf",
+  },
+  {
+    name: "GDPR - Adatkezelési hozzájárulási nyilatkozat",
+    href: "/documents/GDPR%20nyilatkozat.pdf",
+  },
+  {
+    name: "1 %",
+    href: "/documents/2022_adoszam.pdf",
+  },
+  {
+    name: "Egyesületi igazolás: Mindennapos testnevelés alóli felmentési kérelem",
+    href: "/documents/BASE-igazolt-sportolo-adatlap.pdf",
+  },
+  {
+    name: "Egyesületi igazolás: Iskola időben történő rendkívüli edzésen, versenyen való részvételre igazolás/ kikérő",
+    href: "/documents/base_kikero_igazolas.doc",
+  },
+  {
+    name: "BASE körlogó",
+    href: "/documents/BASE-logo-elore.pdf",
+  },
+  {
+    name: "BASE hátoldali logó",
+    href: "/documents/BASE-felirat-hatra.tif",
+  },
+  {
+    name: "BASE színes logó",
+    href: "/documents/BASE-logo-szines.tif",
   },
 ];
 
