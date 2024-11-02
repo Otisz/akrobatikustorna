bdc681c652c57a069032c162517a320d51b508bf
Levente Otta <leventeotta@gmail.com>
2024-11-02 19:13:30 +0100

    Fresh NextJs 15

--- src/lib/sanity.image.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "@/lib/sanity.client";
-import type { SanityImageSource } from "@sanity/asset-utils";
-import imageUrlBuilder from "@sanity/image-url";
-import type { Image } from "sanity";
-
-const builder = imageUrlBuilder(client);
-
-export const urlFor = (source: SanityImageSource | Image) => builder.image(source);
