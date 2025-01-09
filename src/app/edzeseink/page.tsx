723c208367cba8d9b4a7817f97e99abbd9b7e23c
Levente Otta <leventeotta@gmail.com>
2025-01-09 14:59:49 +0100

    Remove WP

--- src/app/edzeseink/page.tsx
+++ src/app/edzeseink/page.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 
-import { getPageBySlug } from "@/lib/wordpress";
-
 export const revalidate = 0;
 
 export const metadata: Metadata = {
@@ -10,16 +8,11 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const page = await getPageBySlug("edzeseink");
-
   return (
     <main className="container pt-8">
       <h1 className="text-4xl font-bold">Edzéseink</h1>
       <section className="mt-8">
-        <div
-          className="prose prose-lg hover:prose-tr:bg-primary/5 prose-table:text-nowrap prose-td:p-4 prose-td:align-middle prose-th:h-12 prose-th:px-4 prose-th:text-left prose-th:align-middle prose-th:font-medium relative w-full max-w-none overflow-auto"
-          dangerouslySetInnerHTML={{ __html: page.content.rendered }}
-        />
+        <div className="prose prose-lg relative w-full max-w-none overflow-auto prose-table:text-nowrap hover:prose-tr:bg-primary/5 prose-th:h-12 prose-th:px-4 prose-th:text-left prose-th:align-middle prose-th:font-medium prose-td:p-4 prose-td:align-middle" />
       </section>
     </main>
   );
