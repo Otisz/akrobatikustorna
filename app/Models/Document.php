f048d49409b912966c421c97ba9739c7c08dfc69
Levente Otta <leventeotta@gmail.com>
2025-08-19 18:39:18 +0200

    Filament - Documents

---
+++ app/Models/Document.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\DocumentObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+#[ObservedBy(DocumentObserver::class)]
+class Document extends Model
+{
+    use HasUuids;
+}
