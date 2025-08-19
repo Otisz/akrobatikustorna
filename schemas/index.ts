89ce2e7bba1c9641953b1bea1f3cc8fb9c4d5359
Levente Otta <leventeotta@gmail.com>
2025-08-19 17:09:05 +0200

    New Laravel

--- schemas/index.ts
+++
@@ -1,4 +0,0 @@
-import news from "./news";
-import trainers from "./trainers";
-
-export const schemaTypes = [news, trainers];
--- src/app/(admin)/studio/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Studio } from "@/app/(admin)/studio/[[...index]]/Studio";
-
-export const dynamic = "force-static";
-
-export { metadata, viewport } from "next-sanity/studio";
-
-export default function StudioPage() {
-  return <Studio />;
-}
